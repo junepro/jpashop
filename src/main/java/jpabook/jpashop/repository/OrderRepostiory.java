bde5316fa46c99cf71dcbe8345021e9985e93814
junepro <57428866+junepro@users.noreply.github.com>
2021-09-27 18:43:22 +0900

    검색기능

--- src/main/java/jpabook/jpashop/repository/OrderRepostiory.java
+++ src/main/java/jpabook/jpashop/repository/OrderRepostiory.java
@@ -16,14 +16,18 @@ public class OrderRepostiory {
 
     public void save(Order order) {
         em.persist(order);
-
     }
-
     public Order findOne(Long id) {
         return em.find(Order.class, id);
     }
 
-//    public List<Order> findAll(OrderSearch orderSearch) {
-//
-//    }
+    public List<Order> findAll(OrderSearch orderSearch) {
+        return em.createQuery("select o from Order o join o.member m" +
+                        " where o.status=:status" +
+                        "and m.name like : name", Order.class)
+                .setParameter("status",orderSearch.getOrderStatus())
+                .setParameter("name",orderSearch.getMemberName())
+                .setMaxResults(1000)//최대 1000건
+                .getResultList();
+    }
 }
